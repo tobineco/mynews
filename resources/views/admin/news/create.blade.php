26df5ff3f21799200d31e7eaadf677f53a216461
tobineco <feimao.318@gmail.com>
2019-10-17 12:20:26 +0000

    8th PHP

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -9,5 +9,23 @@
     </head>
     <body>
         <h1>Myニュース作成画面</h1>
+        
+    {{-- layouts/admin.blade.phpを読み込む --}}
+    @extends('layouts.admin')
+        
+    {{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+    @section('title','ニュースの新規作成')
+
+    {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>ニュース新規作成</h2>
+                </div>
+            </div>
+        </div>   
+    @endsection
+        
     </body>
 </html>
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -9,5 +9,23 @@
     </head>
     <body>
         <h1>Myプロフィール作成画面</h1>
+        
+    {{-- layouts/profile.blade.phpを読み込む --}}
+    @extends('layouts.profile')
+        
+    {{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+    @section('title','プロフィールの新規作成')
+
+    {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィール新規作成</h2>
+                </div>
+            </div>
+        </div>   
+    @endsection
+        
     </body>
 </html>
